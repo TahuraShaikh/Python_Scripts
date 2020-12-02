a4309a476a3b0714874675bb83d83037e8eba893
Tahura <itahura@gmail.com>
2020-12-02 23:53:30 +0100

    prime

---
+++ prime number.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number: "))
+prime= True
+for i in range (2,n):
+    if (n%i==0):
+        prime=False
+        break
+if prime:
+    print(f"{n} is a prime number")
+else:
+    print(f"{n} is not a prime number")
