bb7c435c335a58612e3b628c2c8234b2bfcefe12
Tahura <itahura@gmail.com>
2020-12-06 20:39:55 +0100

    comments added

--- pattern.py
+++ pattern.py
@@ -1,13 +1,13 @@
 #drawing random pattern:
-import turtle
-import random
+import turtle     # turtle library
+import random     #random library
 
-cols=['blue','green','blue','red']
+cols=['blue','green','blue','red']             # list of colours to use
 
-turtle.speed(3)
+turtle.speed(3)            # speed of turtle
 
-for c in range(20):
-    x=random.randint(-100,100)
+for c in range(50):          # loop can be changed
+    x=random.randint(-100,100)      # variables x & y with random values from -100 to 100
     y=random.randint(-100, 100)
-    turtle.color(random.choice(cols))
-    turtle.goto(x,y)
+    turtle.color(random.choice(cols))        # turtle is set to random color
+    turtle.goto(x,y)     # turtle will goto random x & y values
