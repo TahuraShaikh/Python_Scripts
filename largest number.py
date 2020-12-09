badbefe5393476322e294278f5002275b781b0d3
Tahura <itahura@gmail.com>
2020-12-09 17:28:01 +0100

    commented

--- largest number.py
+++ largest number.py
@@ -1,12 +1,13 @@
-def largest(n1,n2,n3,n4):
-    if (n1>n2) and (n1>n3) and (n1>n4):
+#
+def largest(n1,n2,n3,n4):    # can compare 3 numbers also
+    if (n1>=n2) and (n1>=n3) and (n1>=n4):
         return n1
-    elif (n2>n3)and(n2>n4):
+    elif (n2>=n3)and(n2>=n4):
         return n2
-    elif (n3>n4):
+    elif (n3>=n4):
         return n3
     else:
         return n4
 
-a=largest(1,2,3,4)
+a=largest(7,3,2,1)
 print(a)
