51f3486a30f2860e99e053dc66d7f12b5f479646
Tahura <itahura@gmail.com>
2020-12-19 13:56:36 +0100

    added len fun

--- vector_add_real_img.py
+++ vector_add_real_img.py
@@ -23,7 +23,12 @@ class Vector:
             sum+=self.vec[i] + vec2.vec[i]      # multiplying and then adding values of list
         return sum
 
+    def __len__(self):
+        return len(self.vec)
+
 v1=Vector([2,4,6])
 v2=Vector([1,3,6])
 print(v1+v2)
 print(v1*v2)
+print(len(v1))
+print(len(v2))
