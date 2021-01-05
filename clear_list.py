46dcc8df7076722e42b3b548678eb2d2cc7e11b0
Tahura <itahura@gmail.com>
2021-01-05 20:47:18 +0100

    clear a list

---
+++ clear_list.py
@@ -0,0 +1,13 @@
+#to clear all elements of a given list
+l1=[2,4,6,3,2,1]
+print (f'Before clearing : {l1}')
+l1.clear()
+print (f'After clearing : {l1}')
+
+
+#Taking i\p from user:
+l1=input("enter a list")
+l2=list(l1)
+print (f'Before clearing : {l2}')
+l2.clear()
+print(f'After clearing : {l2}')
